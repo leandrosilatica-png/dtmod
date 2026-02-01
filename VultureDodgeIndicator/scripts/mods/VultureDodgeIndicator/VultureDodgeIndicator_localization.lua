63b0fd412e9ca636ac2822dc8b5ec91829fa6f9b
chronic <leandrosilatica@gmail.com>
2026-02-02 07:56:05 +1000

    Create VultureDodgeIndicator_localization.lua file

---
+++ VultureDodgeIndicator/scripts/mods/VultureDodgeIndicator/VultureDodgeIndicator_localization.lua
@@ -0,0 +1,32 @@
+return {
+    mod_name = {
+        en = "Vulture Dodge Indicator",
+    },
+    mod_description = {
+        en = "Shows whether Vulture's Dodge is active.",
+    },
+    show_active = {
+        en = "Show Active State",
+    },
+    show_inactive = {
+        en = "Show Inactive State",
+    },
+    show_in_hub = {
+        en = "Show in Hub",
+    },
+    font_size = {
+        en = "Font Size",
+    },
+    offset_x = {
+        en = "Horizontal Offset",
+    },
+    offset_y = {
+        en = "Vertical Offset",
+    },
+    indicator_active = {
+        en = "Vulture Dodge: ACTIVE",
+    },
+    indicator_inactive = {
+        en = "Vulture Dodge: INACTIVE",
+    },
+}
